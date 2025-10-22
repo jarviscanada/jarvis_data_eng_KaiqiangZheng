0684cf4015dacc33786a484077f72a186dda999e
Kaiqiang Zheng <163541914+KaiqiangZheng1@users.noreply.github.com>
2025-10-21 21:02:49 -0400

    Comment out test sample INSERT statements
    Comment out sample INSERT statements for host_info and host_usage.

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -31,11 +31,11 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
 );
 
 --Only for TEST SAMPLES
-INSERT INTO host_info (id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, "timestamp", total_mem)
-VALUES(4, 'noe3', 1, 'x86_64', 'Intel(R) Xeon(R) CPU @ 2.30GHz', 2300, 256, '2019-05-29 17:49:53.000', 601324);
+--INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, "timestamp", total_mem)
+--VALUES('noe3', 1, 'x86_64', 'Intel(R) Xeon(R) CPU @ 2.30GHz', 2300, 256, '2019-05-29 17:49:53.000', 601324);
 
-INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
-VALUES('2019-05-30 15:01:00.000', 1, 250000, 90, 4, 2, 3);
+--INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
+--VALUES('2019-05-30 15:01:00.000', 1, 250000, 90, 4, 2, 3);
 
 -- Print
 DO $$
